51cbbcfc9d5058ed1cb680802f0e8da71ed719ce
timho <timho1047@gmail.com>
2025-11-15 00:53:36 +0800

    train q 3000

--- assignment3/scripts/train_eval.sh
+++ assignment3/scripts/train_eval.sh
@@ -1,6 +1,8 @@
-RUN_NAME="q_3000
+export CUDA_VISIBLE_DEVICES=5
 
-PATH_TO_THE_MERGED_MODLE="ckpt/$RUN_NAME/models/model_iter_0-merged"
+RUN_NAME="q_3000"
+
+PATH_TO_THE_MERGED_MODLE="ckpt/$RUN_NAME/models/model_iter_1-merged"
 
 bash scripts/self_train_gsm8k.sh Qwen3-0.6B \
     --run_name $RUN_NAME \
@@ -12,10 +14,9 @@ bash scripts/run_gsm8k_eval.sh $PATH_TO_THE_MERGED_MODLE \
     --max_tokens 512 \
     --top_p 0.95 \
     --top_k 20 \
-    --n_rollouts 1 \
+    --n_rollouts 1
 
-bash scripts/run_gsm8k_eval.sh \
-    $PATH_TO_THE_MERGED_MODLE \
+bash scripts/run_gsm8k_eval.sh $PATH_TO_THE_MERGED_MODLE \
     --output_dir results/${RUN_NAME}_8 \
     --temperature 0.6 \
     --max_tokens 512 \
