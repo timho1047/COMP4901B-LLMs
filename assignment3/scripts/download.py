e6d564f5bf2355ea44ca3f2a24101924cf068415
Tim HO <94374732+timho1047@users.noreply.github.com>
2025-11-14 15:19:24 +0800

    remove mistral

--- assignment3/scripts/download.py
+++ assignment3/scripts/download.py
@@ -2,4 +2,4 @@ from huggingface_hub import snapshot_download
 
 snapshot_download(repo_id='Qwen/Qwen3-0.6B', repo_type='model', local_dir='Qwen3-0.6B', cache_dir='.cache')
 snapshot_download(repo_id='gsm8k', repo_type='dataset', local_dir='gsm8k-local', cache_dir='.cache')
-snapshot_download(repo_id='unsloth/mistral-7b-bnb-4bit', repo_type='model', local_dir='mistral-7b-bnb-4bit', cache_dir='.cache')
+# snapshot_download(repo_id='unsloth/mistral-7b-bnb-4bit', repo_type='model', local_dir='mistral-7b-bnb-4bit', cache_dir='.cache')
